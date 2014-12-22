e15321f96a2b2c6ebaeefe2711645c6e18106f9d
brijeshmeshiya <meshiya_brijesh@yahoo.com>
2014-12-22 18:37:34 +0530

    Recursive solution to 9.4 is Added

--- 9 Recursion And Dynamic Programming/9-4.cpp
+++ 9 Recursion And Dynamic Programming/9-4.cpp
@@ -3,29 +3,26 @@
 
 using namespace std;
 
-void print_iterative(int *array,int size)
+string s[1024];		// Set it Equal to nos. of solutions
+
+void print_recursive(int *array,int current,int size)
 {
-	int i,j,k;
-	for(i=0;i<pow(2,size);i++)
-	{
-		j=0;
-		k=i;
-		cout<<"{";
-		while(j<size)
-		{
-			if(k%2==1)
-				cout<<array[j]<<",";
-			k=k>>1;
-			j++;
-		}
-		cout<<"} "<<endl;
-	}
-	cout<<endl;
-}
+	if(current>=size)
+		return;
+	int i=pow(2,current),j,k;
+	for(j=0;j<i;j++)
+		s[j+i]=s[j];
+	for(j=0;j<i;j++)
+		s[j+i]=s[j+i]+(char)(*(array+current)+48)+",";
+	print_recursive(array,current+1,size);
+}	
 
 int main()
 {
 	int a[]={2,3,5,9,1},size=sizeof(a)/sizeof(int);
-	print_iterative(a,size);
+	s[0]="{";
+	print_recursive(a,0,size);
+	for(int i=0;i<pow(2,size);i++)
+		cout<<s[i]<<"}"<<endl;
 	return 1;
 }
